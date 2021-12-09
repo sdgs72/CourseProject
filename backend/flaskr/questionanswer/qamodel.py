c3e8739422009d7f4da599755e41f7e97863aaf0
Dion Hiananto <sdgs72@hotmail.com>
2021-12-09 20:35:46 +0700

    summarization

--- backend/flaskr/questionanswer/qamodel.py
+++ backend/flaskr/questionanswer/qamodel.py
@@ -4,12 +4,20 @@ import random
 import re
 from .nltk_implementation import getAnswerFor
 
+
+REMOVABLE_SECTIONS = ["== See Also ==", "== External links ==", "== References ==", "== Further reading ==", "=== References ==="]
+
+def removeSections(raw_string_page_contents):
+    a_string = raw_string_page_contents
+    for elem in REMOVABLE_SECTIONS:
+        split_string = a_string.split(elem, 1)
+        a_string = split_string[0]
+    return a_string
+
 def cleanText(raw_string_page_contents):
     cleaned_text = raw_string_page_contents.strip()
     cleaned_text = re.sub("=.*=.", "", cleaned_text) # remove headings...
-    print("------------ before cleaning ----------------")
     print(raw_string_page_contents)
-    print("------------ after cleaning ----------------")
     print(cleaned_text)
     return cleaned_text
 
