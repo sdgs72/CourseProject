32c214d42b01cd8fff5172151248b66f0201cc2c
Dion Hiananto <sdgs72@hotmail.com>
2021-12-10 13:52:28 +0700

    UI Fix attempt

--- backend/README.md
+++ backend/README.md
@@ -1,6 +1,6 @@
 # Backend Setup
 
-1. Have python 3.7.x/3.8.x installed
+1. Have python 3.8.x installed
 2. python packages necessary to run are located in requirements.txt, run pip3 install -r requirements.txt to install necessary packages
 3. run the setup.py script, IE python3 setup.py
 4. use ./run to run the flask server (this will use port 5000, please make sure that port 5000 is not used by anyone, we don't support env port loading since the front end is hardcoded to look into port 5000 specficly)
--- extension/popup.js
+++ extension/popup.js
@@ -15,7 +15,9 @@ $(document).ready(function () {
   });
 
   // display raw content
-  chrome.storage.sync.get(null, (items) => {
+  chrome.storage.sync.get("summary", (items) => {
+    console.log("Storage Sync get done...")
+    console.log(items)
     var storageContent = items;
     $("#raw-content").append(
       "<pre>" + JSON.stringify(storageContent, null, 2) + "</pre>"
@@ -47,33 +49,47 @@ $(document).ready(function () {
 
   var queryInput = $("#queryInput");
   var answerContent = $("#answer-content");
+  var queryDisplay = $("#queryDisplayText");
   queryInput.focus();
   queryInput.on("keypress", (e) => {
     if (e.which == 13) {
+      var questionText = queryInput.val()
+      if(questionText == "") {
+        answerContent.html("Please enter a non empty question...")
+        return 
+      }
       console.log(e.target.value);
       console.log("this is windows", window.location.href);
       var value = e.target.value.trim();
-
-      chrome.storage.sync.set({
-        q: value,
-      });
-      e.target.value = "";
-      answerContent.html("Loading ...");
-      setTimeout(() => {
-        location.reload();
-      }, 1500);
-
-      answerContent.html("Loading ...");
+      queryDisplay.html(questionText)
+      answerContent.html("Loading ... ")
+      fetch(
+        "http://localhost:5000/api/qa?" +
+          new URLSearchParams({
+            q: questionText,
+            wiki_url: "https://en.wikipedia.org/wiki/United_States",
+          }),
+        {
+          method: "GET",
+          headers: new Headers({
+            "Content-Type": "application/json",
+          }),
+        }
+      )
+        .then((r) => r.text())
+        .then((response) => {
+          json = JSON.parse(response)
+          console.log(json.result)
+          answerContent.html("")
+          var htmlStr = `<li>${json.result[0].text}</li>`;
+          answerContent.append(htmlStr);          
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+        e.target.value = "";
     }
   });
 
-  chrome.storage.sync.get(["answers"], (e) => {
-    if (e.answers && e.answers.result) {
-      console.log("e.answers", e.answers);
-      e.answers.result.forEach((a) => {
-        var htmlStr = `<li>${a.text}</li>`;
-        answerContent.append(htmlStr);
-      });
-    }
-  });
+
 });
